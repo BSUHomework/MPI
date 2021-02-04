7cec99b4daa455196b5f8653e7aa96a18ddd5289
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-02-05 02:27:42 +0300

    Update task3_pipeline.py

--- task3/task3_pipeline.py
+++ task3/task3_pipeline.py
@@ -10,24 +10,20 @@
 
 from mpi4py import MPI
 
-
-
-
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-
 if rank == 0:
-    data = 10+rank
+    data = 10 + rank
     send_req = comm.isend(data, dest=1)
     send_req.wait()
 else:
-    data = rank*10                  #Data pre-calculation
-    recv_req = comm.irecv(source=rank-1)
-    recv_data=recv_req.wait()
-    data = recv_data+data           #Get the transmitted data and merge them
-    print("this is rank {} result {}".format(rank,data))
-    if rank<size-1:
-        send_second_req = comm.isend(data, dest=rank+1)
+    data = rank * 10  # Data pre-calculation
+    recv_req = comm.irecv(source=rank - 1)
+    recv_data = recv_req.wait()
+    data = recv_data + data  # Get the transmitted data and merge them
+    print("this is rank {} result {}".format(rank, data))
+    if rank < size - 1:
+        send_second_req = comm.isend(data, dest=rank + 1)
         send_second_req.wait()
