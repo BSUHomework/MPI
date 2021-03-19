b54962a56dfcd819c3bdf595787b349ffad3899a
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-03-19 18:30:05 +0300

    Update task3_pipeline.py

--- task3/task3_pipeline.py
+++ task3/task3_pipeline.py
@@ -28,28 +28,36 @@ if __name__ == '__main__':
     '''
 
     if rank == 0:
-        a = np.zeros([4, 4])
+        a = np.zeros([4, 4],dtype=np.int)
         a[0] = [0, 3, 4, 5]
-        send_a_req = comm.isend(a, dest=1)# at 0 proc send 0 array a  to 1 proc
+        send_a_req = comm.isend(a, dest=1)  # at 0 proc send 0 array a  to 1 proc
         send_a_req.wait()
-        for j in range(len(a[0])): # send subarray b[0...4]  to 1 proc
-            send_req = comm.isend(a[0][j], dest=1, tag=j)
-            send_req.wait()
+        for j in range(len(a[0])):  # send subarray b[0...4]  to 1 proc
+            send_req = comm.Isend([a[0][j],MPI.INT], dest=1, tag=j)
+            # send_req.wait()
 
     else:
+
         recv_req = comm.irecv(source=rank - 1)
         a = recv_req.wait()
         print("recv a:\n", a)
-        for j in range(len(a[0])):
-            recv_req = comm.irecv(source=rank - 1, tag=j)
-            recv_data = recv_req.wait()
-            a[rank][j] = recv_data + 1
+        recv_data = np.zeros(1,dtype=np.int)
+        request = comm.Irecv([recv_data,MPI.INT],source=rank - 1, tag=0)
+        for j in range(len(a[0]+1)):
+
+            request.Wait()
+
+            a[rank][j] = recv_data[0] + 1
+
             print(f"compute a[{a[rank][j]}]:", rank, j)
+            request = comm.Irecv([recv_data,MPI.INT],source=rank - 1, tag=j+1)
         if rank < size - 1:
             send_a_second_req = comm.isend(a, dest=rank + 1)
             send_a_second_req.wait()
             for j in range(len(a[0])):
-                send_second_req = comm.isend(a[rank][j], dest=rank + 1, tag=j)
-                send_second_req.wait()
+                # send_second_req = comm.isend(a[rank][j], dest=rank + 1, tag=j)
+                send_second_req = comm.Isend([a[rank][j], MPI.INT], dest=rank + 1, tag=j)
 
-    print("final array:\n", a)
+
+    
+    
