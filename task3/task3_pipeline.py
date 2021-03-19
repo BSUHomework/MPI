0c35a9327133fb68120760d1a4b74d5c67f35a4e
强 <oncwnuGZ7aiQpSybzcK6TnLAgkjI@git.weixin.qq.com>
2021-03-19 11:10:44 +0300

    Update task3_pipeline.py

--- task3/task3_pipeline.py
+++ task3/task3_pipeline.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 '''
 @author: soliva
-@Site: 
+@Site:
 @file: task3_pipeline.py
 @time: 2021/2/4
 @desc:
@@ -30,20 +30,18 @@ if __name__ == '__main__':
     if rank == 0:
         a = np.zeros([4, 4])
         a[0] = [0, 3, 4, 5]
-        send_a_req = comm.isend(a, dest=1)
+        send_a_req = comm.isend(a, dest=1)# at 0 proc send 0 array a  to 1 proc
         send_a_req.wait()
-        for j in range(len(a[0])):
+        for j in range(len(a[0])): # send subarray b[0...4]  to 1 proc
             send_req = comm.isend(a[0][j], dest=1, tag=j)
             send_req.wait()
 
     else:
         recv_req = comm.irecv(source=rank - 1)
-
         a = recv_req.wait()
         print("recv a:\n", a)
         for j in range(len(a[0])):
             recv_req = comm.irecv(source=rank - 1, tag=j)
-
             recv_data = recv_req.wait()
             a[rank][j] = recv_data + 1
             print(f"compute a[{a[rank][j]}]:", rank, j)
@@ -54,4 +52,4 @@ if __name__ == '__main__':
                 send_second_req = comm.isend(a[rank][j], dest=rank + 1, tag=j)
                 send_second_req.wait()
 
-    print("final array:\n",a)
+    print("final array:\n", a)
